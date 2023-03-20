ba8178ce8909c8182278b42c134f911038af5c47
Sauniel Alberto Rodriguez Vargas <sauniel93@gmail.com>
2023-03-20 17:50:49 -0400

    Add topnavbar component and style

--- book-store/src/App.js
+++ book-store/src/App.js
@@ -2,12 +2,21 @@ import Content from "./components/Content";
 import TopNavbar from "./components/TopNavbar";
 import "./index.css";
 
-
 function App() {
   return (
     <div className="App">
-      <TopNavbar/>
-      <Content/>
+      <div
+        className="w-full h-full"
+        style={{
+          background:
+            "linear-gradient(to top, lightgrey 0%, lightgrey 1%, #e0e0e0 26%, #efefef 48%, #d9d9d9 75%, #bcbcbc 100%)",
+        }}
+      >
+        <div className="w-full" style={{ height: "auto", marginBottom: "2px" }}>
+          <TopNavbar />
+        </div>
+        <Content />
+      </div>
     </div>
   );
 }
