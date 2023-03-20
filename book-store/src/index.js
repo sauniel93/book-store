be9f81a9abb2eb03dfffaa63e61759483e5ed024
Sauniel Alberto Rodriguez Vargas <sauniel93@gmail.com>
2023-03-20 19:53:35 -0400

    add book context

--- book-store/src/index.js
+++ book-store/src/index.js
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { ContextProvider } from "./contexts/MenuContext";
+import { BookContextProvider } from "./contexts/BookContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <ContextProvider>
-      <App />
+      <BookContextProvider>
+        <App />
+      </BookContextProvider>
     </ContextProvider>
   </React.StrictMode>
 );
